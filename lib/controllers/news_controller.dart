c6b2b3a9dec29d22e0163b22ffed47851f87503b
ammaar12 <muhammadfarroos12@gmail.com>
2026-01-14 14:19:29 +0700

    refactor NewsController and HomePage for improved state management; implement error handling and loading indicators; add NewsResponseModel for API response

--- lib/controllers/news_controller.dart
+++ lib/controllers/news_controller.dart
@@ -1,22 +1,48 @@
+import 'dart:developer';
+
+import 'package:get/get.dart';
 import 'package:get/state_manager.dart';
+import 'package:news_app/models/news_response_model.dart';
 import 'package:news_app/services/news_service.dart';
 
 class NewsController extends GetxController {
   final NewsService service = NewsService();
 
-  var title = 'Judul berita kemarin'.obs;
-  var isLoading = false.obs;
+  final _isLoading = false.obs;
+  final _articles = <Article>[].obs;
+  final _error = ''.obs;
 
-  Future fetchNews() async {
+  // getter
+  bool get isLoading => _isLoading.value;
+  List<Article> get articles => _articles.toList();
+  String get error => _error.value;
 
+  @override
+  void onInit() {
+    super.onInit();
+    fetchNews();
+  }
+
+  Future<void> fetchNews() async {
     try {
-    isLoading.value = true;
-    title.value = await service.getTopHeadlines();
-    } catch (e) {
-     title.value = 'Gagal mengambil berita karena $e'; 
-    } finally {
-    isLoading.value = false;
-    }
+      _isLoading.value = true;
+      _error.value = '';
 
+      final response = await service.getTopHeadlines();
+
+      _articles.value = response.articles ?? [];
+      log('API returned: ${response.articles?.length}');
+    } catch (e) {
+      log('error: ${e.toString()}');
+      _error.value = e.toString();
+
+      Get.snackbar(
+        'Error',
+        'Failed to load news: ${e.toString()}',
+        snackPosition: SnackPosition.BOTTOM,
+      );
+    } finally {
+      _isLoading.value = false;
+    }
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:news_app/controllers/news_controller.dart';
@@ -7,7 +9,7 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    NewsController controller = NewsController();
+    NewsController controller = Get.put<NewsController>(NewsController());
 
     return Scaffold(
       appBar: AppBar(title: Text('Home Page')),
@@ -15,29 +17,26 @@ class HomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('ini adalah halaman home page'),
-            SizedBox(height: 50),
-
-            FilledButton(
-              onPressed: () {
-                Get.toNamed('/detail');
-              },
-              child: Text('ke halaman detail'),
-            ),
-            SizedBox(height: 50),
-            Obx(() {
-              if (controller.isLoading.value) {
-                return CircularProgressIndicator();
-              }
-              return Center(child: Text(controller.title.value));
-            }),
-            SizedBox(height: 50),
-
-            FilledButton(
-              onPressed: () {
-                controller.fetchNews();
-              },
-              child: Text('munculkan data berita'),
+            Text('Home page'),
+            Expanded(
+              child: Obx(() {
+                log('data yang ada sebanyak: ${controller.articles.length}');
+                if (controller.isLoading) {
+                  return CircularProgressIndicator();
+                }
+                log('data yang ada sebanyak: ${controller.articles.length}');
+                return ListView.builder(
+                  itemCount: controller.articles.length,
+                  itemBuilder: (context, index) {
+                    final article = controller.articles[index];
+                    log('message: ${article.title}');
+                    return ListTile(
+                      title: Text(article.title ?? '-'),
+                      subtitle: Text(article.description ?? 'No Description'),
+                    );
+                  },
+                );
+              }),
             ),
           ],
         ),
